4e34585096f2bb5225afeb76d06be7e1be1e3f8e
archieQa <luarsarsi@gmail.com>
2024-03-23 20:11:04 +0100

    all

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function About() {
     return (
-        <div>About</div>
+        <section id='about-us'>
+             <div>About</div>
+        </section>
     )
 }
 
