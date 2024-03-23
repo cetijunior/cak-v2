bf322118d51b093472b5182a0ac8237cce7e39ca
Cetijunior <lameceti1@gmail.com>
2024-03-23 22:14:50 +0100

    navbar index and hero video

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,15 +3,17 @@ import Image from 'next/image'
 
 function Navbar() {
     return (
-        <div className='p-4 flex flex-row items-center justify-between w-screen'>
-            <h1 className='text-2xl font-bold text-[#446AF2]'>CAK Web Solutions</h1>
+        <div className=' flex flex-row z-10 items-center bg-[#F8F9FB]  justify-between w-screen'>
+            <div className='p-4 flex flex-row z-10 items-center bg-[#F8F9FB]  justify-between w-screen'>
+                <h1 className='text-2xl z-10 bg-[#F8F9FB]  font-bold text-[#446AF2]'>CAK Web Solutions</h1>
 
-            <div className='flex flex-row space-x-8'>
-                <h1 className='cursor-pointer text-xl hover:opacity-70'>About Us</h1>
-                <h1 className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</h1>
-                <h1 className='cursor-pointer text-xl hover:opacity-70'>Projects</h1>
-                <h1 className='cursor-pointer text-xl hover:opacity-70'>Contact</h1>
+                <div className='flex z-10 bg-[#F8F9FB]   flex-row space-x-8'>
+                    <h1 className='cursor-pointer text-xl hover:opacity-70'>About Us</h1>
+                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</h1>
+                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Projects</h1>
+                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Contact</h1>
 
+                </div>
             </div>
         </div>
     )
