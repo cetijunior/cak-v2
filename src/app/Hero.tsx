07352160695fd97015cbe14af21eb886942b73ea
Cetijunior <lameceti1@gmail.com>
2024-03-23 20:48:31 +0100

    Navbar and Hero

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -1,8 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Hero() {
     return (
-        <div className='p-10'>Hero</div>
+        <div>
+            <Image className='mt-[-40px] w-screen' src='/hero.png' width={1920} height={1080} alt={''} />
+
+            <div className='flex flex-col items-center justify-center w-screen'>
+                <div className='w-[600px] h-[400px] bg-[#F8F9FB] shadow-custom-blue rounded-xl mt-[-280px] p-10 space-y-4'>
+                    <h1 className='text-center text-5xl font-bold'>
+                        World-Clas Website Development
+                    </h1>
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, non error.
+                        Suscipit nobis id excepturi repudiandae itaque ipsa quia at sequi in. Ipsam doloremque tempore, nisi eveniet ex consequatur earum?
+                    </h3>
+                </div>
+                <button className='bg-[#446AF2] text-white mt-[-20px] py-3 px-20 rounded-3xl'>Contact Us</button>
+            </div>
+        </div>
     )
 }
 
--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Navbar() {
     return (
-        <div className='p-10'>Navbar</div>
+        <div className='p-4 flex flex-row items-center justify-between w-screen'>
+            <h1>CAK Web</h1>
+
+            <div className='flex flex-row space-x-8'>
+                <h1 className='cursor-pointer text-xl hover:opacity-70'>About Us</h1>
+                <h1 className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</h1>
+                <h1 className='cursor-pointer text-xl hover:opacity-70'>Projects</h1>
+                <h1 className='cursor-pointer text-xl hover:opacity-70'>Contact</h1>
+
+            </div>
+        </div>
     )
 }
 
