6d4a4c82eae6f617a5578332990fa87029879aeb
Cetijunior <lameceti1@gmail.com>
2024-03-23 22:46:38 +0100

    hero update

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -5,20 +5,16 @@ import HeroV from '../../videos/HeroV.mp4'
 function Hero() {
     return (
         <div className='w-screen'>
-            <div className='flex mt-[-600px] justify-center'>
-                <Video
-                    style={{ width: 1980, height: 1080, objectFit: "fill" }}
-                    src={HeroV}
-                    autoPlay
-                    muted
-                    loop
-                    playsInline
-                    controls={false}
-                >
-                </Video>
+            <div className="flex bg-[#1E1919 mt-[-350px] items-center justify-center">
+                <video autoPlay loop muted className="rounded-lg w-screen">
+                    <source src="https://www.helmes.com/wp-content/uploads/2022/07/costom-software-development_600.mp4"
+                        type="video/mp4"
+                    />
+                    Your Browser does not support the video tag.
+                </video>
             </div>
             <div className='flex flex-col items-center justify-center w-screen'>
-                <div className='w-[600px] z-10 h-[400px] bg-[#F8F9FB] shadow-custom-blue mt-[-300px] rounded-xl p-10 space-y-4'>
+                <div className='w-[700px] p-20 z-10 h-[400px] bg-[#F8F9FB] shadow-custom-blue mt-[-300px] rounded-xl  space-y-4'>
                     <h1 className='text-center text-5xl font-bold'>
                         World-Clas Website Development
                     </h1>
