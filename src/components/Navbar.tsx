918d733a35a88ac94bc53d1d88d191e06cd850a8
Cetijunior <lameceti1@gmail.com>
2024-03-24 19:31:27 +0100

    Mininavbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,11 +17,9 @@ function Navbar() {
     ];
 
     const languageLinks = [
-        { href: '', title: 'ENG' },
         { href: '', title: 'DE' },
         { href: '', title: 'AL' },
     ];
-
 
     const scrollToSection = (sectionId: string) => {
         const section = document.getElementById(sectionId);
@@ -39,30 +37,44 @@ function Navbar() {
             {/* Menu/Close Button at the top for small screens */}
             <div className='pr-4 mb-[-10px] z-30 sm:hidden'>
                 <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    {/* Update the path here to correctly point to the images in the public folder */}
-                    {isMenuOpen ? <img src='/close.png' alt='Close' className="w-10 h-10" /> : <img src='/menu.png' alt='Menu' className="w-10 h-10" />}
+                    {isMenuOpen ?
+                        <img src='/close.png' alt='Close' className="w-10 h-10" onClick={navigateBack} />
+                        :
+                        <Link href='/navbar/MiniNavbar'>
+                            <img src='/menu.png' alt='Menu' className="w-10 h-10" />
+                        </Link>
+                    }
                 </button>
+
             </div>
 
             {/* Desktop Navbar */}
             <div className='hidden sm:flex flex-row items-center sm:px-10 space-x-8'>
                 <div className='hidden sm:flex flex-row items-center space-x-8'>
-                    <h1 className='cursor-pointer hover:text-[#446AF2] text-xl z-20' onClick={() => scrollToSection('about-us')}>About Us</h1>
-                    <h1 onMouseEnter={() => setIsDropdownOpenServices(true)} onMouseLeave={() => setIsDropdownOpenServices(false)} onClick={() => scrollToSection('services')} className='cursor-pointer hover:text-[#446AF2] text-xl z-20 relative'>
+                    <h1 className='cursor-pointer hover:text-[#446AF2] text-xl z-20' onClick={() => scrollToSection('about')}>About Us</h1>
+                    <h1 onClick={() => scrollToSection('services')}
+                        onMouseEnter={() => setIsDropdownOpenServices(true)}
+                        className='cursor-pointer hover:text-[#446AF2] text-xl z-20 h-100p relative'>
                         Web Development Services
                         {isDropdownOpenServices && (
-                            <div className='absolute bg-[#f0efef] shadow-custom-blue rounded-md'>
-                                {serviceLinks.map((link) => (
-                                    <Link legacyBehavior key={link.href} href={link.href}>
-                                        <div className='flex flex-row items-center w-full px-2 space-x-3'>
-                                            <img src={link.img} alt={link.title} className="items-start mp-4" width={20} height={20} />
-                                            <a className='items-center py-2 text-gray-700 hover:text-[#446AF2]'>{link.title}</a>
-                                        </div>
-                                    </Link>
-                                ))}
+                            <div onMouseLeave={() => setIsDropdownOpenServices(false)} className='absolute top-full left-1/2 transform -translate-x-1/2 mt-10 w-[700px] bg-[#ffffff] shadow-custom-blue rounded-md p-4'>
+                                <div className='grid grid-cols-2 gap-4'>
+                                    {serviceLinks.map((link, index) => (
+                                        <Link legacyBehavior key={index} href={link.href}>
+                                            <a className='flex items-center space-x-3 bg-white p-2 rounded-lg hover:shadow-custom-blue'>
+                                                <img src={link.img} alt={link.title} className="w-10 h-10" />
+                                                <div>
+                                                    <div className='font-semibold text-gray-900'>{link.title}</div>
+                                                    <div className='text-sm text-gray-600'>{link.description}</div>
+                                                </div>
+                                            </a>
+                                        </Link>
+                                    ))}
+                                </div>
                             </div>
                         )}
                     </h1>
+
                     <h1 className='cursor-pointer hover:text-[#446AF2] text-xl z-20' onClick={() => scrollToSection('projects')}>Projects</h1>
                     <h1 className='cursor-pointer hover:text-[#446AF2] text-xl z-20' onClick={() => scrollToSection('contact')}>Contact</h1>
 
@@ -83,22 +95,7 @@ function Navbar() {
 
                 </div>
 
-                {/* Fullscreen Menu for small screens */}
-                <div className={`fixed inset-0 bg-[#2a3f8b] z-20 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out sm:hidden`}>
-                    <ul className='flex h-full flex-col w-[150px] pl-10 space-y-20 items-start justify-center text-white'>
-                        <li className='cursor-pointer text-2xl hover:opacity-70'>About Us</li>
-                        <div className='flex flex-row items-center space-x-36 '>
-                            <li className='cursor-pointer text-2xl hover:opacity-70'>ENG</li>
-                            <img src='/arrow.png' alt='next' className='w-10 h-10 rotate-180 hover:opacity-70'></img>
-                        </div>
-                        <li className='cursor-pointer text-2xl hover:opacity-70'>Projects</li>
-                        <li className='cursor-pointer text-2xl hover:opacity-70'>Contact</li>
-                        <div className='flex flex-row items-center space-x-36 '>
-                            <li className='cursor-pointer text-2xl '>ENG</li>
-                            <img src='/arrow.png' alt='next' className='w-10 h-10 rotate-180 hover:opacity-70'></img>
-                        </div>
-                    </ul>
-                </div>
+
             </div>
         </div>
     );
