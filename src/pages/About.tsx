cd772835815b61850fbc1a1ba65d27790d389419
kenidervishaj <krenar.dervishaj@fau.de>
2024-03-24 22:04:02 +0100

    About Fixes and Contact form

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -48,10 +48,10 @@ function About() {
 
     return (
         <section ref={aboutRef} id='about' className='bg-white mx-auto min-h-[600px]'>
-            <div className='flex flex-col space-x-8 sm:ml-[-80px] ml-0 justify-center items-center' style={{ opacity: show[0] ? 1 : 0, transition: 'opacity 1s ease' }}>
-                <div className='flex flex-col sm:mt-32 sm:gap-10 gap-7'>
-                    <h2 className=' text-center lg:text-3xl text-2xl font-semibold z-10'>Who are we?</h2>
-                    <p className='lg:text-left text-sm lg:text-base text-center w-[300px] md:text-center lg:w-[400px] z-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit architecto modi dolor, atque laborum qui ea dolorem cupiditate fugit harum corrupti veniam. Blanditiis repudiandae earum id laboriosam omnis quas voluptate!</p>
+            <div className='flex flex-col space-y-28 sm:ml-[-80px] ml-0 items-center' style={{ opacity: show[0] ? 1 : 0, transition: 'opacity 1s ease' }}>
+                <div className='flex flex-col sm:mt-32 sm:gap-20 gap-7'>
+                    <h2 className=' text-center lg:text-[64px] text-2xl font-semibold z-10'>Who are we?</h2>
+                    <p className='lg:text-center text-base lg:text-base text-center w-[300px]  md:text-center lg:w-[400px] z-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit architecto modi dolor, atque laborum qui ea dolorem cupiditate fugit harum corrupti veniam. Blanditiis repudiandae earum id laboriosam omnis quas voluptate!</p>
                 </div>
                 <CarouselAbout />
             </div>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Contact() {
     return (
-        <section id='contact'>Contact</section>
+        <section id='contact' className='bg-white mx-auto py-10 min-h-[600px]'>
+            <div className='flex flex-wrap items-start justify-center'>
+                <div className='flex flex-col bg-[#E6F0F8] rounded-[39px] h-[700px] w-[650px]'>
+                   <p className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi aspernatur voluptate possimus, labore aliquid dicta consequatur delectus? Consequuntur quis sint nam fugiat repellendus, distinctio dolorem dolores omnis provident autem velit?</p> 
+                </div>
+                <div className='bg-white width-[300px] h-[500px]'>
+
+                </div>
+            </div>
+        </section>
     )
 }
 
