028701d1711178c9923ff1aba33d9c6b85834f30
Cetijunior <lameceti1@gmail.com>
2024-03-24 14:40:11 +0100

    navbar

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -5,7 +5,7 @@ import HeroV from '../../videos/HeroV.mp4'
 function Hero() {
     return (
         <div className='w-screen'>
-            <div className="flex bg-[#1E1919 mt-[-350px] items-center justify-center">
+            <div className="flex bg-[#1E1919 sm:mt-[-350px] items-center justify-center">
                 <video autoPlay loop muted className="rounded-lg w-screen">
                     <source src="https://www.helmes.com/wp-content/uploads/2022/07/costom-software-development_600.mp4"
                         type="video/mp4"
@@ -14,13 +14,17 @@ function Hero() {
                 </video>
             </div>
             <div className='flex flex-col items-center justify-center w-screen'>
-                <div className='w-[700px] p-20 z-10 h-[400px] bg-[#F8F9FB] shadow-custom-blue mt-[-300px] rounded-xl  space-y-4'>
+                <div className='sm:w-[650px] p-20 z-10 sm:h-[400px] bg-[#F8F9FB] shadow-custom-blue  sm:mt-[-350px] rounded-xl space-y-4'>
                     <h1 className='text-center text-5xl font-bold'>
                         World-Clas Website Development
                     </h1>
-                    <h3>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, non error.
-                        Suscipit nobis id excepturi repudiandae itaque ipsa quia at sequi in. Ipsam doloremque tempore, nisi eveniet ex consequatur earum?
+                    <h3 className='text-center'>
+                        Discover our range of services
+                        and find out how we can help you
+                        take your project to the next level.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Officiis, quasi, in cupiditate vero dicta eligendi maiores id ut quidem, illo molestiae alias quisquam magnam?
+                        Minima perferendis consectetur delectus culpa? Perspiciatis!
                     </h3>
                 </div>
                 <button className='bg-[#446AF2] z-20 text-white mt-[-20px] py-3 px-20 rounded-3xl'>Contact Us</button>
--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,22 +1,36 @@
-import React from 'react'
+'use client'
 
+import React, { useState } from 'react';
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <div className=' flex flex-row z-10 items-center bg-[#F8F9FB]  justify-between w-screen'>
-            <div className='p-4 flex flex-row z-10 items-center bg-[#F8F9FB]  justify-between w-screen'>
-                <h1 className='text-2xl z-10 bg-[#F8F9FB]  font-bold text-[#446AF2]'>CAK Web Solutions</h1>
+        <div className='relative flex items-center justify-between bg-[#F8F9FB] w-screen'>
+            <div className='p-4'>
+                <h1 className='text-2xl font-bold text-[#446AF2]'>CAK Web Solutions</h1>
+            </div>
 
-                <div className='flex z-10 bg-[#F8F9FB]   flex-row space-x-8'>
-                    <h1 className='cursor-pointer text-xl hover:opacity-70'>About Us</h1>
-                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</h1>
-                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Projects</h1>
-                    <h1 className='cursor-pointer text-xl hover:opacity-70'>Contact</h1>
+            <div className='pr-4'>
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='sm:hidden'>
+                    {/* Conditional text for Menu Button */}
+                    <span>{isMenuOpen ? 'Close' : 'Menu'}</span>
+                </button>
+            </div>
 
-                </div>
+            {/* Adjusted classes for dropdown effect */}
+            <div className={`absolute top-full mt-4 right-0 shadow-custom-blue bg-[#F8F9FB] flex-col items-start space-y-4 p-4 rounded sm:static 
+                             sm:bg-transparent sm:shadow-none sm:flex sm:flex-row sm:space-y-0 sm:space-x-8 sm:p-0 ${isMenuOpen ? 'flex' : 'hidden'}`}
+            >
+                <ul>
+                    <li className='cursor-pointer text-xl hover:opacity-70'>About Us</li>
+                    <li className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</li>
+                    <li className='cursor-pointer text-xl hover:opacity-70'>Projects</li>
+                    <li className='cursor-pointer text-xl hover:opacity-70'>Contact</li>
+                </ul>
             </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
