471a05ab2048cf7292327874597aa5d3996a5833
kenidervishaj <krenar.dervishaj@fau.de>
2024-03-24 15:28:36 +0100

    Responsive on phones and monitors

--- src/app/CarouselAbout.tsx
+++ src/app/CarouselAbout.tsx
@@ -10,9 +10,9 @@ function CarouselAbout() {
   const [buttonState, setButtonState] = React.useState("visible");
   const [animationKey, setAnimationKey] = React.useState(0);
   const items = [
-    { name: 'Krenar Dervishaj', role: 'Co-Founder, FullStack Developer and UI/UX Designer', className:'rounded-full ml-2 mt-14 w-[300px] h-[300px]', img: '/keniPFP.png', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?' },
-    { name: 'Shefqet CJ Lame', role: 'Co-Founder and FullStack Developer', img: '/cjPFP.png', className:'mt-16 rounded-full ml-4 w-[300px] h-[300px]', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?'},
-    { name: 'Luars Qamo', role: 'Co-Founder and FullStack Developer', img: '/archiePFP.png', className:'rounded-3xl mt-[-10px] rounded-full ml-[-44px] w-[400px] h-[400px]', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?'}
+    { name: 'Krenar Dervishaj', role: 'Co-Founder, FullStack Developer and UI/UX Designer', className:'rounded-full w-[150px] h-[150px] ml-[70px] mt-[-60px] sm:ml-2 sm:mt-12 sm:w-[300px] sm:h-[300px]', img: '/keniPFP.png', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?' },
+    { name: 'Shefqet CJ Lame', role: 'Co-Founder and FullStack Developer', img: '/cjPFP.png', className:'rounded-full w-[150px] h-[150px] ml-[70px] mt-[-60px] sm:ml-8 sm:mt-20 sm:w-[250px] sm:h-[250px]', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?'},
+    { name: 'Luars Qamo', role: 'Co-Founder and FullStack Developer', img: '/archiePFP.png', className:'rounded-full w-[200px] h-[200px] ml-[45px] mt-[-65px] sm:ml-2 sm:mt-12 sm:w-[300px] sm:h-[300px]', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?'}
   ];
 
   const triggerFadeEffect = () => {
@@ -62,15 +62,15 @@ function CarouselAbout() {
   };
   
   return (
-    <div>
-      <button onClick={goToPrevSlide} className={`button-fade z-10 sm:left-[495px] left-3 mt-[325px] sm:mt-[170px] absolute w-10 h-10 bg-white shadow-custom-blue rounded-full ${buttonState === "visible" || buttonState === "fading in" ? "opacity-1" : "opacity-0"} `}>Left</button>
-      <button onClick={goToNextSlide} className={` button-fade  z-10 sm:right-20 right-3 mt-[325px] sm:mt-[170px] absolute w-10 h-10 bg-white shadow-custom-blue rounded-full ${buttonState === "visible" || buttonState === "fading in" ? "opacity-1" : "opacity-0"} `}>Right</button>
-      <div key={animationKey} className={`sm:flex sm:flex-row flex flex-col sm:gap-10 gap-10 split-bg-vertically sm:bgForAbout rounded-[39px] shadow-custom-blue ${getAnimationClass(current)}`}>
+    <div className='mt-20 w-[290px] sm:w-[800px]'>
+      <button onClick={goToPrevSlide} className={`button-fade z-10 sm:left-[615px] left-8 mt-[190px] sm:mt-[190px] absolute w-10 h-10 bg-white shadow-custom-blue rounded-full ${buttonState === "visible" || buttonState === "fading in" ? "opacity-1" : "opacity-0"} `}>Left</button>
+      <button onClick={goToNextSlide} className={` button-fade z-10 sm:right-20 right-8 mt-[190px] sm:mt-[190px] absolute w-10 h-10 bg-white shadow-custom-blue rounded-full ${buttonState === "visible" || buttonState === "fading in" ? "opacity-1" : "opacity-0"} `}>Right</button>
+      <div key={animationKey} className={`sm:flex sm:flex-row flex flex-col sm:gap-10 gap-10 split-bg-vertically sm:bgForAbout relative sm:right-[-125px] rounded-[39px] shadow-custom-blue ${getAnimationClass(current)}`}>
                     <img src={items[current].img} alt={items[current].name} className={items[current].className}/>
-                    <div className='px-4 mb-6'>
-                        <p className='text-center sm:text-left'>{items[current].text}</p>
+                    <div className='px-4 sm:py-28 py-6 flex flex-col sm:items-start items-center'>
+                        <p className='text-center text-xs sm:w-[300px] w-[200px] sm:text-sm sm:text-left'>{items[current].text}</p>
                         <h1 className='sm:text-left text-center font-semibold mt-4'>{items[current].name}</h1>
-                        <h1 className='sm:text-left text-center text-sm'>{items[current].role}</h1>
+                        <h1 className='sm:text-left text-center text-sm mt-2 '>{items[current].role}</h1>
                     </div>
                 </div>
       
