183443e4c176c4084b67bf5e7f7a9a24a6366117
Cetijunior <lameceti1@gmail.com>
2024-03-24 15:32:47 +0100

    navbar and hero responsive

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -5,7 +5,7 @@ import HeroV from '../../videos/HeroV.mp4'
 function Hero() {
     return (
         <div className='w-screen'>
-            <div className="flex bg-[#1E1919 sm:mt-[-350px] items-center justify-center">
+            <div className="flex bg-[#1E1919 mt-[-50px] sm:mt-[-350px] items-center justify-center">
                 <video autoPlay loop muted className="rounded-lg w-screen">
                     <source src="https://www.helmes.com/wp-content/uploads/2022/07/costom-software-development_600.mp4"
                         type="video/mp4"
@@ -14,8 +14,8 @@ function Hero() {
                 </video>
             </div>
             <div className='flex flex-col items-center justify-center w-screen'>
-                <div className='sm:w-[650px] p-20 z-10 sm:h-[400px] bg-[#F8F9FB] shadow-custom-blue  sm:mt-[-350px] rounded-xl space-y-4'>
-                    <h1 className='text-center text-5xl font-bold'>
+                <div className='w-[400px] sm:w-[650px] p-20 z-10 sm:h-[400px] bg-[#F8F9FB] shadow-custom-blue mt-[-40px] sm:mt-[-350px] rounded-xl space-y-4'>
+                    <h1 className='text-center text-2xl sm:text-5xl font-bold'>
                         World-Clas Website Development
                     </h1>
                     <h3 className='text-center'>
@@ -27,7 +27,7 @@ function Hero() {
                         Minima perferendis consectetur delectus culpa? Perspiciatis!
                     </h3>
                 </div>
-                <button className='bg-[#446AF2] z-20 text-white mt-[-20px] py-3 px-20 rounded-3xl'>Contact Us</button>
+                <button className='bg-[#446AF2] z-10 text-white mt-[-20px] py-3 px-20 rounded-3xl'>Contact Us</button>
             </div>
         </div>
     )
--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react';
 
@@ -6,27 +6,44 @@ function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div className='relative flex items-center justify-between bg-[#F8F9FB] w-screen'>
+        <div className='relative sm:px-10 flex items-center justify-between bg-[#F8F9FB] w-screen'>
             <div className='p-4'>
                 <h1 className='text-2xl font-bold text-[#446AF2]'>CAK Web Solutions</h1>
             </div>
 
-            <div className='pr-4'>
-                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='sm:hidden'>
-                    {/* Conditional text for Menu Button */}
-                    <span>{isMenuOpen ? 'Close' : 'Menu'}</span>
+            {/* Menu/Close Button at the top for small screens */}
+            <div className='pr-4 z-30 sm:hidden'>
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    {/* Update the path here to correctly point to the images in the public folder */}
+                    {isMenuOpen ? <img src='/close.png' alt='Close' className="w-6 h-6" /> : <img src='/menu.png' alt='Menu' className="w-6 h-6" />}
                 </button>
             </div>
 
-            {/* Adjusted classes for dropdown effect */}
-            <div className={`absolute top-full mt-4 right-0 shadow-custom-blue bg-[#F8F9FB] flex-col items-start space-y-4 p-4 rounded sm:static 
-                             sm:bg-transparent sm:shadow-none sm:flex sm:flex-row sm:space-y-0 sm:space-x-8 sm:p-0 ${isMenuOpen ? 'flex' : 'hidden'}`}
-            >
-                <ul>
+            {/* Fullscreen Menu for small screens */}
+            <div className={`fixed inset-0 bg-[#2a3f8b] z-20 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out sm:hidden`}>
+                <ul className='flex h-full flex-col w-[150px] pl-10 space-y-20 items-start justify-center text-white'>
+                    <li className='cursor-pointer text-2xl hover:opacity-70'>About Us</li>
+                    <div className='flex flex-row items-center space-x-16 '>
+                        <li className='cursor-pointer text-wrap text-2xl hover:opacity-70'>Web Development Services</li>
+                        <button>Pick</button>
+                    </div>
+                    <li className='cursor-pointer text-2xl hover:opacity-70'>Projects</li>
+                    <li className='cursor-pointer text-2xl hover:opacity-70'>Contact</li>
+                    <div className='flex flex-row items-center space-x-36 '>
+                        <li className='cursor-pointer text-2xl hover:opacity-70'>ENG</li>
+                        <button className=''>Pick</button>
+                    </div>
+                </ul>
+            </div>
+
+            {/* Navbar list for larger screens */}
+            <div className='hidden sm:flex flex-row items-center space-x-8 pr-4'>
+                <ul className='flex flex-row space-x-8 text-[#3c3d41]'>
                     <li className='cursor-pointer text-xl hover:opacity-70'>About Us</li>
                     <li className='cursor-pointer text-xl hover:opacity-70'>Web Development Services</li>
                     <li className='cursor-pointer text-xl hover:opacity-70'>Projects</li>
                     <li className='cursor-pointer text-xl hover:opacity-70'>Contact</li>
+                    <li className='cursor-pointer text-xl hover:opacity-70'>ENG</li>
                 </ul>
             </div>
         </div>
