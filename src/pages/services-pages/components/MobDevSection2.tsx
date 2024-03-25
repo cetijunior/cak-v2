72f05e09f2dfe1d630584f40cad2a74d00b87afe
archieQa <luarsarsi@gmail.com>
2024-03-25 23:27:30 +0100

    did the mobdev page

--- src/pages/services-pages/components/MobDevSection2.tsx
+++ src/pages/services-pages/components/MobDevSection2.tsx
@@ -49,6 +49,46 @@ const MobDevSection2 = () => {
           </div>
         </div>
       </div>
+      <div className="h-[50rem] flex flex-row justify-center items-center gap-20 space-x-10">
+        <div className="space-y-7">
+          <h1 className="text-3xl font-serif">
+            Our customers find our
+            <br /> services exceptional
+          </h1>
+          <p>
+            We don’t just develop high-end mobile apps,<br></br> but we also
+            build business relationships. Our <br></br>early insights and final
+            results routinely exceed<br></br> customer expectations.
+          </p>
+          <p>
+            Our customers find our service quality<br></br> outstanding. We have
+            an NPS twice as<br></br> high as the software industry average.
+          </p>
+          <p>
+            Our top 20 customers have been with us for<br></br> more than 10
+            years on average, trusting us to<br></br> develop their
+            business-critical software.
+          </p>
+          <p>
+            We are often ranked as the best external<br></br> development
+            partner by our customers.
+          </p>
+        </div>
+        <img
+          className="h-[30rem] w-[50rem]"
+          src="https://www.helmes.com/wp-content/uploads/2021/10/Helmes_Design_Central-1.jpg"
+        ></img>
+      </div>
+      <div className="flex justify-center items-center h-[20rem] bg-blue-200 ">
+        <div className="flex flex-col justify-center items-center  space-y-10">
+          <h1 className="text-3xl">Contact us</h1>
+          <p className="text-xl font-thin">
+            Get in touch! Contact any of our people or drop us a line at
+            info@helmes.com.<br></br> Let’s discuss your mobile app development
+            plans.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
