2d47b1ce6bf1f1b4d795ae029005de1b8ccc2041
kenidervishaj <krenar.dervishaj@fau.de>
2024-03-25 22:15:31 +0100

    about, contact

--- src/pages/carousel-about/CarouselAbout.tsx
+++ src/pages/carousel-about/CarouselAbout.tsx
@@ -8,13 +8,13 @@ import "swiper/css/pagination";
 import { Navigation, Pagination } from "swiper/modules";
 
 function CarouselAbout() {
-  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+  const [screenWidth, setScreenWidth] = React.useState(0);
   const items = [
     {
       name: "Krenar Dervishaj",
       role: "Co-Founder, FullStack Developer and UI/UX Designer",
       className:
-        "rounded-full w-[150px] h-[150px] ml-[0px] z-10 mt-[0px] sm:ml-[-30px] sm:w-[300px] sm:h-[300px]",
+        "rounded-full w-[150px] h-[150px] ml-[0px] z-10 mt-[0px] sm:ml-[-40px] sm:w-[300px] sm:h-[300px]",
       img: "/keniPFP.png",
       text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nulla nobis quisquam cum libero praesentium iste doloribus nam, molestias nemo dignissimos. Sed, repudiandae est mollitia repellat sapiente sint totam ea?",
     },
@@ -37,14 +37,17 @@ function CarouselAbout() {
   ];
 
   React.useEffect(() => {
-    const handleResize = () => {
+    // Update screenWidth with the actual window width after mount
+    setScreenWidth(window.innerWidth);
+
+    const updateWindowWidth = () => {
       setScreenWidth(window.innerWidth);
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", updateWindowWidth);
 
-    // Clean up
-    return () => window.removeEventListener("resize", handleResize);
+    // Clean up the event listener when the component unmounts
+    return () => window.removeEventListener("resize", updateWindowWidth);
   }, []);
 
   const backgroundClass =
@@ -57,12 +60,12 @@ function CarouselAbout() {
       <img
         src="/quotes.png"
         alt="quotes"
-        className="sm:h-30 h-[40px] w-[60px] sm:w-40 sm:mt-[-70px] mt-[-20px] ml-[-35px] sm:ml-[-120px] absolute"
+        className="sm:h-[100px] h-[40px] w-[60px] sm:w-40 sm:mt-[-70px] mt-[-20px] ml-[-35px] sm:ml-[-120px] absolute"
       />
       <img
         src="/quotes.png"
         alt="quotes"
-        className="sm:h-30 h-[40px] w-[60px] sm:w-40 sm:mt-[300px] mt-[580px] rotate-180 ml-[265px] sm:ml-[760px] absolute"
+        className="sm:h-[100px] h-[40px]  w-[60px] sm:w-40 sm:mt-[320px] mt-[580px] rotate-180 ml-[265px] sm:ml-[760px] absolute"
       />
       <Swiper
         modules={[Navigation, Pagination]}
@@ -84,12 +87,12 @@ function CarouselAbout() {
               <div
                 className={`flex flex-wrap ${backgroundClass} h-[600px] sm:h-[350px]  py-8 rounded-[39px] space-x-4 relative justify-center items-start`}
               >
-                <div className="absolute bg-custom-blue-gradient z-0 h-[150px] blur-[25px] w-[150px] rounded-full" />
                 <img
                   src={item.img}
                   alt={item.name}
                   className={item.className}
                 />
+                <div className="absolute bg-custom-blue-gradient z-0 h-[150px] sm:h-[300px] sm:w-[200px] sm:right-[530px] blur-[35px] w-[150px] rounded-full" />
                 <div className="px-6 mt-0 sm:mt-10 sm:py-0 rounded-[39px] space-y-4 flex flex-col sm:items-start items-center">
                   <p className="text-center text-[15px] sm:mb-4 mt-8 sm:mt-0 sm:w-[300px] w-[200px] sm:text-sm sm:text-left">
                     {item.text}
