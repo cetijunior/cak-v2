e80936073fbcdde981eee2b925dfd1e6face772d
Cetijunior <lameceti1@gmail.com>
2024-03-26 16:39:48 +0100

    miniNavbar

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -7,9 +7,9 @@ function Portfolio() {
       id="projects"
       className="bg-white mx-auto min-h-[600px]"
     >
-      <div className="flex flex-col sm:space-y-16 mb-10 items-center">
-        <div className="flex flex-col mt-10 sm:mt-32 gap-7">
-          <h2 className=" text-center text-2xl sm:text-7xl font-semibold">
+      <div className="flex flex-col sm:space-y-16 sm:mb-10 items-center">
+        <div className="flex flex-col mt-10 sm:mt-32 ">
+          <h2 className=" text-center text-4xl sm:text-7xl font-semibold">
             What We Do :
           </h2>
         </div>
